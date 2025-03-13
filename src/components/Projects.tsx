206a62286be8c13ab4f231d5466c95890d300236
Full-Stack & Frontend Developer | React, Next.js, .NET, Python, TailwindCSS <dark810moon@gmail.com>
2025-03-13 04:43:06 -0400

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -71,8 +71,8 @@ const Projects: React.FC = () => {
       description: "Make.com scenario where given a drive folder of media (images & videos), it will randomly grab one. GPT-Vision will analyze the media, then GPT-4 will create a tweet from a fine-tuned model. Dependent on if the media is an image or video, it will upload that media to X/Twitter and Telegram. The scenario was designed to randomly reschedule itself during day-time hours for the next day.",
       image: "/images/Auto Media TweetTelegram  Make.com  ChatGPT.png",
       tags: ["Telegram","ChatGPT","AI Builder","API","Automation","Make.com","Twitter/X","System Automation"],
-      liveUrl: "https://dk-realtimechat.app",
-      githubUrl: "https://github.com/darkmoon810/Real-Time-Chat-Application"
+      liveUrl: "https://darkmoonportfolio.vercel.app",
+      githubUrl: "https://darkmoonportfolio.vercel.app"
     },
     {
       id: 7,
@@ -80,8 +80,8 @@ const Projects: React.FC = () => {
       description: "Make.com solution to automatically create a sheet from a bank statement, and given a folder of invoices, attach each invoice with AI to their specific transaction in the sheet.",
       image: "/images/Auto Transaction Matcher  Make.com  OpenAI  Sheets.png",
       tags: ["Make.com","Automation","Google Sheets","System Automation","ChatGPT","OpenAI API","AI Data Analytics"],
-      liveUrl: "https://dk-realtimechat.app",
-      githubUrl: "https://github.com/darkmoon810/Real-Time-Chat-Application"
+      liveUrl: "https://darkmoonportfolio.vercel.app",
+      githubUrl: "https://darkmoonportfolio.vercel.app"
     }
   ];
 
